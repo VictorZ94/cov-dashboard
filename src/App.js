2dd5bdbc072d234e88a3a5d036a4a5eb79ecff52
Victor Zuluaga <zrvictor@outlook.com>
2024-02-20 17:35:42 -0500

    added section sumary card and time series

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
 import './App.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Select from "react-select";
+import Card from "./SummaryCard";
+import { Line } from "react-chartjs-2";
+import Chart from "chart.js/auto";
 
 function App() {
+    const [activeLocation, setActiveLocation] = useState("can");
+    const [lastUpdated, setlastUpdated] = useState("");
+    const [summaryData, setSummaryData] = useState({});
+    const [timeseriesData, setTimeseriesData] = useState({
+        datasets: [],
+      });
+
+    const getSummaryData = async () => {
+        let res;
+        if (activeLocation === "can") {
+            res = await fetch(`${baseUrl}/summary?geo=${activeLocation}`);
+        } else {
+            res = await fetch(`${baseUrl}/summary?loc=${activeLocation}`);
+        }
+        let resData = await res.json();
+        console.log(resData)
+        let summaryData = resData.data[0];
+    
+        console.log(summaryData)
+        let formattedData = {};
+     
+        Object.keys(summaryData).map(
+          (key) => (formattedData[key] = summaryData[key].toLocaleString())
+        );
+        console.log(formattedData)
+        setSummaryData(formattedData);
+      };
 
   const locationList = [
     { value: "AB", label: "Alberta" },
@@ -21,6 +51,33 @@ function App() {
     { value: "YT", label: "Yukon" },
   ];
 
+  useEffect(() => {
+    getSummaryData();
+    getVersion();
+  }, [activeLocation]);
+
+  const baseUrl = "https://api.opencovid.ca";
+  const getVersion = async () => {
+    const res = await fetch(`${baseUrl}/version`);
+    const data = await res.json();
+    setlastUpdated(data.summary);
+  };
+
+  const timeseriesOptions = {
+    responsive: true,
+    normalized: true,
+    plugins: {
+      tooltip: {
+        enabled: false,
+      },
+    },
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        min: 0,
+      },
+    },
+  };
 
   return (
     <div className="App">
@@ -29,12 +86,38 @@ function App() {
         <div className="dashboard-menu">
         <Select
             options={locationList}
+            onChange={(selectedOption) =>
+              setActiveLocation(selectedOption.value)
+            }
+            defaultValue={locationList.filter(
+              (options) => options.value === activeLocation
+            )}
             className="dashboard-select"
           />
-          <p className="update-date"> Last Updated :  </p>
+          <p classNa me="update-date">
+            Last Updated : {lastUpdated}
+          </p>
         </div>
-        <div className="dashboard-timeseries"></div>
-        <div className="dashboard-summary"></div>
+        <div className="dashboard-timeseries">
+        <Line
+            data={timeseriesData}
+            options={timeseriesOptions}
+            className="line-chart"
+          />
+        </div>
+        <div className="dashboard-summary">
+        <Card title="Total Cases" value={summaryData.cases} />
+          <Card
+            title="Total Recovered"
+            value="not provided"
+          />
+          <Card title="Total Deaths" value={summaryData.deaths} />
+          <Card
+            title="Total Vaccinated"
+            value={summaryData.vaccine_administration_total_doses}
+          />
+            
+        </div>
       </div>
     </div>
   );
